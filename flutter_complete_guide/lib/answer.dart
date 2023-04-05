85da09227aff9c937924c61dc844b1c7ba0d306d
unknown <ainbalqis0305@gmail.com>
2023-04-05 15:45:30 +0800

    first commit

---
+++ flutter_complete_guide/lib/answer.dart
@@ -0,0 +1,27 @@
+/* AIN BALQIS KHALIDA BINTI JAMIL KHIR
+   209966
+*/
+
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: Colors.blue,
+          textStyle: TextStyle(color: Colors.white),
+        ),
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
